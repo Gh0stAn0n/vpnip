9dc6ce19f69f33c3330ff5d35294f868579e8af4
gh0st <102325071+Gh0stAn0n@users.noreply.github.com>
2023-03-08 13:12:15 -0900

    Update vpnip.sh

--- vpnip.sh
+++ vpnip.sh
@@ -11,15 +11,15 @@ check=$(cat ~/.bashrc)
 
 	if [[ $check = *vpnip* ]]
 	then
-	echo -e " ${ORANGE}-${STOP} vpnip command has been added ${LGREEN}already${STOP}, file removal in progress..."
+	echo -e " ${ORANGE}-${STOP} vpnip command has been added already, file removal in progress..."
 	echo ""
 	echo -e " ${ORANGE}-${STOP} if you encounter any issue with the command, go to: ${RED}https://github.com/Gh0stAn0n/vpnip/issues ${STOP}"
 
 	else
 	chmod +x vpnip
 	mv vpnip /usr/bin/vpnip
-
 	echo "alias vpnip='/usr/bin/vpnip'" >> ~/.bashrc
+	
         echo -e " ${ORANGE}-${STOP} vpnip command has been added, file removal in progress..."
         echo ""
         echo -e " ${ORANGE}-${STOP} restart the terminal by closing it, and type ${ORANGE}'${STOP}${LGREEN}vpnip${STOP}${ORANGE}'${STOP} to activate."
